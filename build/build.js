b8fec9361d942cb1cf335a24028cd0197498a6be
BeneGCZ <57296176+BeneGCZ@users.noreply.github.com>
2026-07-26 17:46:46 +0200

    update

--- build/build.js
+++
@@ -1,1 +0,0 @@
-const fs = require("fs"); const path = require("path"); const dist = path.join(__dirname, "../dist"); if (!fs.existsSync(dist)) fs.mkdirSync(dist, {recursive: true}); console.log("Building...");
